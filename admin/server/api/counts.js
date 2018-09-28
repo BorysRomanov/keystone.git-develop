3359266dd9c0cd62e45e6bb9aa30a590be24ac1e
Borys_R <borys.romanov.91@gmail.com>
2018-09-28 15:03:11 +0300

    Fixed Background counts

--- admin/server/api/counts.js
+++ admin/server/api/counts.js
@@ -4,10 +4,17 @@ module.exports = function (req, res) {
 	var keystone = req.keystone;
 	var counts = {};
 	async.each(keystone.lists, function (list, next) {
-		list.model.count(function (err, count) {
-			counts[list.key] = count;
-			next(err);
-		});
+		if (list.key === 'Background') {
+			list.model.find({removed: false}).count(function (err, count) {
+				counts[list.key] = count;
+				next(err);
+			});
+		} else {
+			list.model.count(function (err, count) {
+				counts[list.key] = count;
+				next(err);
+			});
+		}
 	}, function (err) {
 		if (err) return res.apiError('database error', err);
 		return res.json({
